55435e22b7c95efa03ec46b1d4cc47f86a36adb4
Tim Oltman <toltman@gmail.com>
2022-11-03 20:59:47 -0500

    create some simple tables

--- R/table1.R
+++ R/table1.R
@@ -5,3 +5,28 @@ library(here)
 df <- read_excel(here::here("Data", "Data 2020-21", "TS2.xlsx"))
 
 head(df)
+
+df %>%
+    summarise(PartFundNO = sum(PartFundNO))
+
+status <- df %>%
+    summarise(
+        NewPNO = sum(NewPNO),
+        ConPNO = sum(ConPNO),
+    )
+
+status <- df %>%
+    summarise(across(NewPNO:ConPNO, sum))
+status
+
+gender <- df %>%
+    summarise(across(MalePNO:FemalePNO, sum))
+gender
+
+raceth <- df %>%
+    summarise(across(AmIndPNO:UnknownPNO, sum))
+raceth
+
+eligib <- df %>%
+    summarise(across(LowFirstPNO:OtherPNO, sum))
+eligib
